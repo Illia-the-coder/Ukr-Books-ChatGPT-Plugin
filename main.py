363e303620d8fc3e9ea0d819b9adc27903276b69
illia Liudohovskyi <101904816+Illia-the-coder@users.noreply.github.com>
2023-06-21 16:38:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -12,39 +12,57 @@ app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.c
 async def list_all(grade, type):
     db = DB(grade, type)
     all_books = {'list all':db.list_all()}
-    # all_books.['Main web-site view']=f'https://translate.google.com/translate?sl=uk&tl=en&u=https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grades'
+    all_books['Main web-site view eng']=f'https://translate.google.com/translate?sl=uk&tl=en&u=https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade'
+    all_books['Main web-site view ukr']=f'https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade'
     return quart.Response(response=json.dumps(all_books), mimetype="application/json", status=200)
 
-@app.post("/get_books/<string:grade>/<string:type>")
-async def get_books(grade, type):
+@app.post("/get_books/<string:grade>/<string:type>/<string:author>/")
+async def get_books(grade, type, author):
     db = DB(grade, type)
-    books = db.list_all()
+    books = {'books':df.get_books(author)}
+    index = db.authors.index(author)
+    books['Main web-site view eng']=f'https://translate.google.com/translate?sl=uk&tl=en&u=https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/auth_ind={index}'
+    books['Main web-site view ukr']=f'https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/auth_ind={index}'
+
     return quart.Response(response=json.dumps(books), mimetype="application/json", status=200)
 
 @app.post("/get_presentation/<string:grade>/<string:type>")
 async def get_presentation(grade, type):
     db = DB(grade, type)
     presentation = db.get_presentation()
+    presentation['Main web-site view eng']=f'https://translate.google.com/translate?sl=uk&tl=en&u=https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/pres'
+    presentation['Main web-site view ukr']=f'https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/pres'
     return quart.Response(response=json.dumps(presentation), mimetype="application/json", status=200)
 
 @app.post("/get_bio/<string:grade>/<string:type>/<string:author>")
 async def get_bio(grade, type, author):
     db = DB(grade, type)
     bio = db.get_bio(author)
-    print(bio)
+    index = db.authors.index(author)
+    bio['Main web-site view eng']=f'https://translate.google.com/translate?sl=uk&tl=en&u=https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/auth_ind={index}'
+    bio['Main web-site view ukr']=f'https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/auth_ind={index}'
     return quart.Response(response=json.dumps(bio), mimetype="application/json", status=200)
 
 @app.post("/get_content/<string:grade>/<string:type>/<string:author>/<string:name>")
 async def get_content(grade, type, author, name):
     db = DB(grade, type)
     content = db.get_content(author, name)
+    index = db.authors.index(author)
+    book_index = df.get_books(author).index(name)
+    content['Main web-site view eng']=f'https://translate.google.com/translate?sl=uk&tl=en&u=https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/auth_ind={index}/book_ind={book_index}'
+    content['Main web-site view ukr']=f'https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/auth_ind={index}/book_ind={book_index}'
     return quart.Response(response=json.dumps(content), mimetype="application/json", status=200)
 
 @app.post("/get_rnd/<string:grade>/<string:type>")
 async def get_rnd(grade, type):
     db = DB(grade, type)
     rnd = db.get_rnd()
+    index = db.authors.index(DB.rnd_auth)
+    book_index = df.get_books(author).index(DB.rnd_book)
+    rnd['Main web-site view eng']=f'https://translate.google.com/translate?sl=uk&tl=en&u=https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/auth_ind={index}/book_ind={book_index}'
+    rnd['Main web-site view ukr']=f'https://literature2.illia56.repl.co/{("ukr" if "ukr" in type else "for")}/{grade}_grade/auth_ind={index}/book_ind={book_index}'
     return quart.Response(response=json.dumps(rnd), mimetype="application/json", status=200)
+
 
 @app.get("/logo.jpg")
 async def plugin_logo():
